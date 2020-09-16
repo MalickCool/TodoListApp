e1364586a79f90b34df616c9c3f003e04e59908e
Malick Coulibaly <coolmalick2@gmail.com>
2020-09-16 12:32:15 +0000

    ReactJS simple TodoList App with LocalStorage

---
+++ src/components/Board.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            change: props,
+        };
+    }
+
+    close = e => {
+        let { index } = this.props;
+        this.props.removeItem(index);
+    }
+
+    changeState = e => {
+        let { index } = this.props;
+        this.props.markAsDone(index);
+    }
+
+    render() {
+        var decoration = this.props.item.finished;
+        return (
+            <div className={'row pt-2 pb-2 pl-1 pr-1 mb-1 boardContainer ' +decoration}>
+                <div className="col-11 text-left" onClick={this.changeState}>
+                    {this.props.item.item}
+                </div>
+                
+                <div className="col-1 pl-0 pr-0">
+                    <i onClick={this.close} className="fa fa-window-close"></i>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Board;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import Board from './Board';
+class TodoList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: "",
+            todolist: []
+        };
+        this.textInput = React.createRef();
+    }
+
+    onChange = e => {
+        this.setState({
+            "item": e.target.value
+        });
+    }
+
+    componentWillMount(){
+
+        
+
+        if (localStorage.todoList) {
+
+            let todolist = JSON.parse(localStorage.todoList);
+            this.setState({ todolist: todolist })
+        }
+    }
+
+    addToList = e => {
+        if(this.state.item) {
+            var { todolist } = this.state;
+            todolist.unshift( {
+                item: this.state.item,
+                finished: false
+            });
+            this.setState({ todolist: todolist });
+            this.setState({
+                "item": ""
+            });
+
+            localStorage.setItem("todoList", JSON.stringify(todolist));
+
+            this.textInput.current.focus();
+        }
+    }
+
+    removeItem = index => {
+        var { todolist } = this.state;
+        todolist.splice(index, 1);
+        this.setState({todolist: todolist});
+        localStorage.setItem("todoList", JSON.stringify(todolist));
+    }
+
+    markAsDone = index => {
+        var { todolist } = this.state;
+        let theTask = todolist[index];
+        let newState = theTask.finished === 'underline' ? '' : 'underline'
+        todolist[index].finished = newState
+        this.setState({todolist: todolist});
+        localStorage.setItem("todoList", JSON.stringify(todolist));
+    }
+
+    render() {
+        var { todolist } = this.state;
+
+        var liste = todolist.map((item, index) => {
+            return (
+                <Board key={index} item={item} index={index} editItem={this.editItem} removeItem={this.removeItem} markAsDone={this.markAsDone} />
+            );
+        });
+
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="offset-md-2 col-md-3 col-12">
+                        <div className="form-group row">
+                            <label htmlFor="item" className="col-md-12 control-label col-form-label text-left"><b>Entrez votre Tâche</b></label>
+                            <div className="col-md-9 pt-3">
+                                <input ref={this.textInput} type="text" id="item" onChange={this.onChange} value={this.state.item} className='form-control'/>
+                            </div>
+                            <div className="col-md-3 pt-3">
+                                <button type="button" className="btn btn-success" onClick={this.addToList}>Valider</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="offset-md-1 col-md-4 col-12 card p-0">
+                        <div className="card-header">
+                            Mes Tâches
+                        </div>
+                        <div className="card-body">
+                            { liste }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TodoList;
